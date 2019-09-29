c7d6d53768dd27eab7642e3d512f54527670273c
MateuszPospiech <pospiechmateusz@gmail.com>
2019-09-29 20:14:06 +0200

    React - added history game

--- src/index.js
+++ src/index.js
@@ -50,12 +50,13 @@ import './index.css';
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -64,16 +65,35 @@ import './index.css';
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history: history.concat([{
-          squares: squares,
+          squares: squares
         }]),
-        xIsNext: !this.state.xIsNext,
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
       });
-  }
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move?
+          'Przejdź do ruchu#' + move :
+          'Przejdź na początek  gry';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
 
       let status;
       if(winner) {
@@ -92,7 +112,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
